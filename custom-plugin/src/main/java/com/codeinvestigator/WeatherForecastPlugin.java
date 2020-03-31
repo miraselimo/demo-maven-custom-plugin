dc1349b1b5a8b89d338c98ca3519300a32f890b2
22374604 <22374604@sun.ac.za>
2020-04-01 01:04:04 +0200

    used MavenProject class to print output directory

--- custom-plugin/src/main/java/com/codeinvestigator/WeatherForecastPlugin.java
+++ custom-plugin/src/main/java/com/codeinvestigator/WeatherForecastPlugin.java
@@ -6,6 +6,7 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 @Mojo(name="weatherforecastTomorrow", defaultPhase = LifecyclePhase.GENERATE_RESOURCES)
 public class WeatherForecastPlugin extends AbstractMojo {
@@ -13,9 +14,13 @@ public class WeatherForecastPlugin extends AbstractMojo {
     @Parameter(defaultValue = "NotSet")
     private String forecast;
 
+    @Parameter(defaultValue = "${project}")
+    private MavenProject project;
+
     public void execute() throws MojoExecutionException, MojoFailureException {
         getLog().info("*** weatherforecastTomorrow plugin BEGIN");
         getLog().info("The forecast for tomorrow is: "+forecast);
+        getLog().info("Project build directory: "+project.getBuild().getOutputDirectory());
         getLog().info("*** weatherforecast Tomorrow plugin END");
     }
 }
